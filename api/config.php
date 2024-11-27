6a09bb9e10a47d0bc334aba3eae387272c63c858
Jhon <jatudillo@openpass.com.ar>
2024-11-26 23:47:10 -0300

    uplo

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+
+// Definir las rutas de los archivos JSON
+define('CATEGORIAS_FILE', 'db/categorias.json');
+define('COMIDAS_FILE', 'db/comidas.json');
+define('BEBIDAS_FILE', 'db/bebidas.json');
+?>
